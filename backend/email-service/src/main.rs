3581905dc5af000ea0fd025a69213df47ae59657
Andrei Serban <andrei.serban@brewingbytes.com>
2025-08-17 00:49:21 +0300

    feat: add activation email

--- backend/email-service/src/main.rs
+++ backend/email-service/src/main.rs
@@ -1,9 +1,8 @@
-use lettre::{SmtpTransport, transport::smtp::authentication::Credentials};
 use tonic::transport::Server;
 
 use crate::{
     config::Config,
-    service::{Service, service::email_service_server::EmailServiceServer},
+    service::{Service, email_service::email_service_server::EmailServiceServer},
 };
 
 mod config;
@@ -14,13 +13,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
     let config = Config::init();
 
-    let creds = Credentials::new(config.smtp_username, config.smtp_password);
-    let _mailer = SmtpTransport::starttls_relay(&config.smtp_relay)?
-        .credentials(creds)
-        .build();
-
     let addr = "[::1]:8082".parse()?;
-    let service = Service::default();
+    let service = Service::new(config.into())?;
 
     println!("Server listening on {}", addr);
     Server::builder()
--- backend/email-service/src/service.rs
+++ backend/email-service/src/service.rs
@@ -1,15 +1,76 @@
+use std::sync::Arc;
+
+use handlebars::Handlebars;
+use lettre::{
+    Message, SmtpTransport, Transport, message::MultiPart,
+    transport::smtp::authentication::Credentials,
+};
+use serde_json::json;
 use tonic::{Request, Response, Result, Status};
 
-use crate::service::service::{
-    ActivateAccountRequest, ActivateAccountResponse, email_service_server::EmailService,
+use crate::{
+    config::Config,
+    service::email_service::{
+        ActivateAccountRequest, ActivateAccountResponse, email_service_server::EmailService,
+    },
 };
 
-pub mod service {
+const ACTIVATE_ACCOUNT_TEMPLATE: &str = include_str!("../emails/activate_account_template.html");
+
+pub mod email_service {
     tonic::include_proto!("email_service");
 }
 
-#[derive(Default)]
-pub struct Service {}
+pub struct Service {
+    config: Arc<Config>,
+    mailer: SmtpTransport,
+}
+
+impl Service {
+    pub fn new(config: Arc<Config>) -> Result<Self, Box<dyn std::error::Error>> {
+        let creds = Credentials::new(config.smtp_username.clone(), config.smtp_password.clone());
+        let mailer = SmtpTransport::starttls_relay(&config.smtp_relay)?
+            .credentials(creds)
+            .build();
+
+        Ok(Self { config, mailer })
+    }
+
+    async fn create_activate_account_mail(
+        &self,
+        request: &ActivateAccountRequest,
+    ) -> Result<Message, ()> {
+        let m = Message::builder()
+            .from(
+                format!("{} <{}>", self.config.smtp_name, self.config.smtp_email)
+                    .parse()
+                    .map_err(|_| ())?,
+            )
+            .to(format!("{} <{}>", request.username, request.email)
+                .parse()
+                .map_err(|_| ())?)
+            .subject("Activate your account");
+
+        let plain = format!(
+            "Use the following link to activate your account: {}",
+            request.link
+        );
+
+        let html = Handlebars::new()
+            .render_template(
+                ACTIVATE_ACCOUNT_TEMPLATE,
+                &json!({"activation_link": request.link}),
+            )
+            .map_err(|_| ())?;
+
+        m.multipart(MultiPart::alternative_plain_html(plain, html))
+            .map_err(|_| ())
+    }
+
+    fn send_email(&self, message: Message) -> Result<(), ()> {
+        self.mailer.send(&message).map_err(|_| ()).map(|_| ())
+    }
+}
 
 #[tonic::async_trait]
 impl EmailService for Service {
@@ -17,7 +78,13 @@ impl EmailService for Service {
         &self,
         request: Request<ActivateAccountRequest>,
     ) -> Result<Response<ActivateAccountResponse>, Status> {
-        println!("Got {:?}", request);
+        let message = self
+            .create_activate_account_mail(&request.into_inner())
+            .await
+            .map_err(|_| Status::internal("Could not create email."))?;
+
+        self.send_email(message)
+            .map_err(|_| Status::internal("Could not send email."))?;
 
         let reply = ActivateAccountResponse { success: true };
         Ok(Response::new(reply))
