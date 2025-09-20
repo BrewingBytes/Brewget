f1c66d9e88b05dae9daf34aa5e96fa6b1f0f049f
Andrei Serban <andrei.serban@brewingbytes.com>
2025-09-20 23:08:14 +0200

    feat: add frontend github ci

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -9,24 +9,11 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AuthGlass: typeof import('./src/components/AuthGlass.vue')['default']
-    AuthView: typeof import('./src/components/AuthView.vue')['default']
     Button: typeof import('primevue/button')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
     IconField: typeof import('primevue/iconfield')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     InputIcon: typeof import('primevue/inputicon')['default']
     InputText: typeof import('primevue/inputtext')['default']
-    LoginGlass: typeof import('./src/components/LoginGlass.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
-  }
-  export interface GlobalDirectives {
-    StyleClass: typeof import('primevue/styleclass')['default']
   }
 }
