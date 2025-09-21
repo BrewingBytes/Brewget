baf2a6ba15329bfd0fd925b60eba8ac43887e2b6
Andrei Serban <andrei.serban@brewingbytes.com>
2025-09-21 17:46:55 +0300

    feat: add ui for forgot password ui (#29)

---
+++ frontend/src/components/ForgotPasswordGlass.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps(["changePasswordId"]);
+
+const password = ref("");
+function buttonAction() { console.error(props); }
+</script>
+
+<template>
+    <div
+        class="px-8 md:px-12 lg:px-20 py-12 flex flex-col items-center gap-12 w-full backdrop-blur-2xl rounded-2xl bg-white/10 border border-white/10 max-w-sm">
+        <div class="flex flex-col items-center gap-4 w-full">
+            <div class="flex flex-col gap-2 w-full">
+                <div class="text-center text-3xl font-medium text-white leading-tight">BrewGet</div>
+            </div>
+        </div>
+        <div class="flex flex-col items-center gap-8 w-full">
+            <div class="flex flex-col gap-6 w-full">
+                <IconField>
+                    <InputIcon class="pi pi-lock text-white/70!" />
+                    <InputText v-model="password" type="password"
+                        class="appearance-none! border! border-white/10! w-full! outline-0! bg-white/10! text-white! placeholder:text-white/70! rounded-3xl! shadow-sm!"
+                        placeholder="Password" />
+                </IconField>
+            </div>
+            <Button @click="buttonAction" label="Change password"
+                class="w-full! rounded-3xl! bg-surface-950! border! border-surface-950! text-white! hover:bg-surface-950/80!" />
+        </div>
+    </div>
+</template>
--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
 import AuthGlass from "@/components/AuthGlass.vue";
+import ForgotPasswordGlass from "@/components/ForgotPasswordGlass.vue";
+
+
+const route = useRoute();
+const isAuth = computed(() => route.name === "login");
 </script>
 
 <template>
     <div class="pa-0 flex items-center justify-center backdrop-blur-3xl bg-cover! bg-center! bg-no-repeat!"
         style="background-image: url('https://fqjltiegiezfetthbags.supabase.co/storage/v1/object/public/block.images/blocks/signin/signin-glass.jpg')">
-        <AuthGlass />
+        <AuthGlass v-if="isAuth" />
+        <ForgotPasswordGlass v-else :changePasswordId="route.params.id" />
     </div>
 </template>
