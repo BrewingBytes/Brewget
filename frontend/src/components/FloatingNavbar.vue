d7632c84263987bbd89e48492de03f5fcacf5763
Andrei Serban <andrei.serban@brewingbytes.com>
2025-09-22 22:05:28 +0200

    fix: fix

--- frontend/src/components/FloatingNavbar.vue
+++ frontend/src/components/FloatingNavbar.vue
@@ -4,10 +4,10 @@ import { useRoute, useRouter } from "vue-router";
 
 const styles = {
     unselectedButtonPt: {
-        root: "lg:!min-w-[100px] lg:!max-w-[100px] !max-w-[80px] !min-w[80-px] !text-xs !rounded-2xl !bg-transparent !border-transparent hover:!bg-gradient-to-br from-white/60 to-blue-200/40",
+        root: "lg:!min-w-[100px] lg:!max-w-[100px] !max-w-[80px] !min-w-[80-px] !text-xs !rounded-2xl !bg-transparent !border-transparent hover:!bg-gradient-to-br from-white/60 to-blue-200/40",
     },
     selectedButtonPt: {
-        root: "lg:!min-w-[100px] lg:!max-w-[100px] !max-w-[80px] !min-w[80-px] !text-xs !rounded-2xl !text-blue-600 !bg-transparent !bg-gradient-to-br from-white/60 to-blue-200/40 !backdrop-blur-md !border-transparent !shadow-[0_2px_8px_rgba(0,0,0,0.15)] !ring-1 !ring-black/5",
+        root: "lg:!min-w-[100px] lg:!max-w-[100px] !max-w-[80px] !min-w-[80-px] !text-xs !rounded-2xl !text-blue-600 !bg-transparent !bg-gradient-to-br from-white/60 to-blue-200/40 !backdrop-blur-md !border-transparent !shadow-[0_2px_8px_rgba(0,0,0,0.15)] !ring-1 !ring-black/5",
     },
     unselectedRoundButtonPt: {
         root: "!max-w-[75px] !text-xs !bg-transparent !border-transparent hover:!bg-gradient-to-br from-white/60 to-blue-200/40",
